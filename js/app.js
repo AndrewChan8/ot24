98d5f93e1efe7ac8a22731fa3af046adc702ad0f
Sophie <sophiez.89@outlook.com>
2024-10-01 09:48:30 -0700

    attempt to dynamically add mappool data on to html file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,2 @@
-const  changeColor = (element) => {
-    element.style.backgroundColor= "#145f48"
-}
+import data from '../scripts/mappools/qf.json' assert { type: 'json' }; // Importing the JSON file
+console.log(data); // Use the JSON data here
