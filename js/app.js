24fefa2b931f4c85d0c0ce4d3d8f50caa9ae0b4a
Sophie <sophiez.89@outlook.com>
2024-10-01 11:01:17 -0700

    Dynamically include mappool

--- js/app.js
+++ js/app.js
@@ -1,2 +1,52 @@
-import data from '../scripts/mappools/qf.json' assert { type: 'json' }; // Importing the JSON file
-console.log(data); // Use the JSON data here
+fetch("../scripts/mappools/qf.json")
+    .then(response => {
+        if(!response.ok) {
+            throw new Error(`HTTP error! Status ${response.status}`)
+        }
+        return response.json();
+    })
+    .then(mappool => {
+        // console.log(mappool);
+        const mappoolCardContainer = document.getElementById("mappoolCardContainer");
+
+        for(const maps in mappool) {
+            let map = mappool[maps];
+            console.log(mappool[maps].beatmap_id)
+            const cardWrapper = document.createElement('div');
+            cardWrapper.classList.add("cardWrapper");
+            cardWrapper.innerHTML = `
+                <div id="mappoolCardTop">
+                    <div id="mapBannerWrapper">
+                        <img id="banner" src="../assets/images/mappoolCard/mappoolBanner.png" alt="${map.title} Banner">
+                        <img id="bannerBorder" src="../assets/images/mappoolCard/bannerBorder.png" alt="Banner Border">
+                        <div id="mapIDWrapper">
+                            <span id="mapID">${map.beatmap_id}</span>
+                            <img id="mapIDBackground" src="../assets/images/mappoolCard/mapID.png" alt="Map ID Background">
+                        </div>
+                    </div>
+                    <div id="mapStats">
+                        <ul>
+                            <li>${map.star_rating}<img class="mapIcon" src="../assets/images/mappoolCard/star.png" alt="Star Icon"></li> 
+                            <li>${map.bpm}<img class="mapIcon" src="../assets/images/mappoolCard/speed.png" alt="Speed Icon"></li>
+                            <li>${map.length}<img class="mapIcon" src="../assets/images/mappoolCard/timeLapse.png" alt="Time Icon"></li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="mapCreator">
+                    <div id="modWrapper">
+                        <span id="mod">${maps}</span>
+                        <img id="modBackground" src="../assets/images/mappoolCard/modBackgroundHR.png" alt="Mod Background">
+                    </div>
+                    <div id="songInfo">
+                        <span id="song">${map.title}</span>
+                        <span id="artist">${map.artist}</span>
+                    </div>
+                </div>
+            `;
+            mappoolCardContainer.appendChild(cardWrapper);
+        }
+    })
+    .catch(error => {
+        console.error("Error Fetching JSON:", error);
+    });
+
