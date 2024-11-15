1f992c609f5c6c355972685beea7901dee5e35bd
sophie <sophiez.89@outlook.com>
2024-11-15 10:34:01 -0800

    added conditionals

--- public/js/statsLoader.js
+++ public/js/statsLoader.js
@@ -2,13 +2,13 @@
 function loadStatData(activeTab) {
     const StatContainer = document.querySelector("#userTableBody");
     const comingSoon = document.querySelector(".coming-soon");
-    
+    const statsTable = document.querySelector("#statsTable");
     fetch(`scripts/stats/${activeTab}.json`)
         .then(response => {
             console.log(response);
             if(!response.ok) {
                 comingSoon.style.display = "block";
-                StatContainer.style.display = "none";
+                statsTable.style.display = "none";
                 return Promise.reject("ERROR");
             }
             console.log(response.json);
@@ -16,14 +16,31 @@ function loadStatData(activeTab) {
         })
         .then(stats => {
            // make table here
+           statsTable.style.display = "block";
+           comingSoon.style.display = "none";
            const tableBody = document.getElementById("userTableBody");
+           tableBody.innerHTML = "";
            stats.forEach((user) => {
                 console.log(user);
                 const row = document.createElement("tr");
                 for(let score in user){
                     const dataCell = document.createElement("td");
                     const dataText = document.createTextNode(user[score]);
+
                     dataCell.appendChild(dataText);
+                    if(score == "username"){
+                        dataCell.classList.add("rowHeader");
+                    } else {
+                        dataCell.classList.add("bodyData");
+                        const percentageValue = parseFloat(user[score]);
+                        if (percentageValue <= 100 && percentageValue >= 70){
+                            dataCell.style.backgroundColor = "#50a77a";
+                        } else if(percentageValue <= 70 && percentageValue >= 60){
+                            dataCell.style.backgroundColor = "red";
+                        }
+                    }
+
+
                     console.log(dataCell);
                     row.appendChild(dataCell);
                 }
