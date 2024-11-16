487ea561be368699aeecb34fb6648e7dd4456b61
sophie <sophiez.89@outlook.com>
2024-11-15 21:59:44 -0800

    blah

--- public/js/statsInit.js
+++ public/js/statsInit.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const defaultTab = "qualifiersStat";
+    const switchBtn = document.querySelector("#placementButton");
     loadStatData(defaultTab);
-
+    
     const tabs = document.querySelectorAll('.nav-link');
     tabs.forEach(tab => {
         tab.addEventListener("click", function(event) {
             event.preventDefault();
             const activeTab = tab.getAttribute("data-active");
-        
             tabs.forEach(t => t.classList.remove("active"));
             tab.classList.add("active");
             loadStatData(activeTab);
-            console.log(activeTab);
         });
     });
+
+    switchBtn.addEventListener("click", function(event) {
+        tabs.forEach(tab => {
+            event.preventDefault();
+            const activeTab = tab.getAttribute("data-active");
+            tabs.forEach(t => t.classList.remove("active"));
+            tab.classList.add("active");
+            console.log("Button clicked")
+        })
+    })
 });
--- public/js/statsLoader.js
+++ public/js/statsLoader.js
@@ -1,12 +1,42 @@
+const comingSoon = document.querySelector(".coming-soon");
+const statsTable = document.querySelector("#statsTable");
+
 function percentageColor(score, dataCell){
     const opacity = Math.round((score * .01) * 100) / 100; 
     dataCell.style.backgroundColor = `rgba(56, 127, 95, ${opacity})`;
     console.log(opacity);
 }
 
+
+function loadPercentageData(stats) {
+    statsTable.style.display = "block";
+    comingSoon.style.display = "none";
+    const tableBody = document.getElementById("userTableBody");
+    tableBody.innerHTML = "";
+    stats.forEach((user) => {
+         console.log(user);
+         const row = document.createElement("tr");
+         for(let score in user){
+             const dataCell = document.createElement("td");
+             const dataText = document.createTextNode(user[score]);
+
+             dataCell.appendChild(dataText);
+             if(score == "username"){
+                 dataCell.classList.add("rowHeader");
+             } else {
+                 dataCell.classList.add("bodyData");
+                 const percentageValue = parseFloat(user[score]);
+                 percentageColor(percentageValue, dataCell);
+             }
+
+             row.appendChild(dataCell);
+         }
+         tableBody.appendChild(row);
+    })
+}
+    
+
 function loadStatData(activeTab) {
-    const comingSoon = document.querySelector(".coming-soon");
-    const statsTable = document.querySelector("#statsTable");
     fetch(`scripts/stats/${activeTab}.json`)
         .then(response => {
             console.log(response);
@@ -19,34 +49,31 @@ function loadStatData(activeTab) {
             return response.json();
         })
         .then(stats => {
-           // make table here
-           statsTable.style.display = "block";
-           comingSoon.style.display = "none";
-           const tableBody = document.getElementById("userTableBody");
-           tableBody.innerHTML = "";
-           stats.forEach((user) => {
-                console.log(user);
-                const row = document.createElement("tr");
-                for(let score in user){
-                    const dataCell = document.createElement("td");
-                    const dataText = document.createTextNode(user[score]);
-
-                    dataCell.appendChild(dataText);
-                    if(score == "username"){
-                        dataCell.classList.add("rowHeader");
-                    } else {
-                        dataCell.classList.add("bodyData");
-                        const percentageValue = parseFloat(user[score]);
-                        percentageColor(percentageValue, dataCell);
-                    }
-
-                    row.appendChild(dataCell);
-                }
-                tableBody.appendChild(row);
-           })
-           
+           // make table 
+           loadPercentageData(stats);
         })
         .catch(error => {
             console.error("Error Fetching JSON:", error);
         });
+}
+
+
+function loadPlacementData(activeTab){
+    console.log(activeTab);
+    fetch(`scripts/stats/${activeTab}`)
+        .then(response => {
+            if(!response.ok) {
+                comingSoon.style.display = "block";
+                statsTable.style.display = "none";
+                return Promise.reject("ERROR");
+            }
+            console.log(response.json);
+            return response.json();
+        })
+        .then(stats => {
+            console.log(stats)
+        })
+        .catch(error => {
+            console.log("Error Fetching JSON", error);
+        })
 }
