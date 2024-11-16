681093301972a49f2c5c9d227fad8df488d0fc12
AndrewSushi <AndrewSushi8@gmail.com>
2024-11-16 02:05:39 -0800

    Added the rankings and the averages

--- public/scripts/root/percentScores.py
+++ public/scripts/root/percentScores.py
@@ -4,16 +4,28 @@ if __name__ == "__main__":
   name = input("Enter mappool name: ")
   mods = []
   results = {}
+  placements = {}
   
   with open(f'../rawScores/{name}.json', 'r') as file:
     results = json.load(file)
+
+  with open(f'../placements/{name}.json', 'r') as file:
+    placements = json.load(file)
   
   with open(f'../other/{name}.json', 'r') as file:
     mods = json.load(file)
   
+
+  rankings = list(placements["rankings"].keys())
+
   percentages = {}
+  resultsList = ["rank", "avg", *mods]
+
   for player in results:
+    index = rankings.index(player)
     temp = {}
+    temp["rank"] = index + 1
+    acc = 0
     for score in results[player]:
       raw_score = results[player][score]
       mod = score[:2]
@@ -26,8 +38,14 @@ if __name__ == "__main__":
         percent = raw_score / 1200000
       
       percent = round((percent * 100), 2)
+      acc += percent
       temp[score] = percent
-    percentages[player] = temp
+    acc /= len(mods)
+    temp["avg"] = round(acc, 2)
+
+    sorted_res = {key: temp[key] for key in resultsList if key in temp}
+
+    percentages[player] = sorted_res
 
   with open(f'../percentages/{name}.json', 'w') as file:
     json.dump(percentages, file, indent=4)
