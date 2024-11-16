298660e20953c84f9ccb504463381331bd0146c9
AndrewSushi <AndrewSushi8@gmail.com>
2024-11-16 03:34:37 -0800

    Removed the name extraction

--- public/scripts/root/extractBeatmapNames.py
+++
@@ -1,9 +0,0 @@
-import json
-
-name = input("Enter mappool name:")
-
-with open(f"../mappools/{name}.json", "r") as outfile:
-  data = json.load(outfile)
-
-for map in data:
-  print(data[map]["title"])
